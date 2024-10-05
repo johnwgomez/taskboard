dc3846a776872e9f34a5d506d6d60ad791ed8128
johnwgomez <johnwgomez10@gmail.com>
2024-10-04 18:33:36 -0700

    finished create a task card todo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,23 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
+// Todo: create a function to generate a unique task id 
 function generateTaskId() {
-
+    return nextId++;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+    return `
+    <div class="task card mb-3" data-id="${task.id}">
+      <div class="card-body">
+        <h5 class="card-title">${task.title}</h5>
+        <p class="card-text">${task.description}</p>
+        <p class="card-text"><small class="text-muted">Due: ${task.deadline}</small></p>
+        <button class="btn btn-danger btn-sm delete-task">Delete</button>
+      </div>
+    </div>
+  `;
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -18,12 +27,12 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
